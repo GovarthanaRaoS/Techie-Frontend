7d4cd4614a100eb8d7a7063312196293bfdeebb8
GovarthanaRaoS <128133771+GovarthanaRaoS@users.noreply.github.com>
2023-08-27 20:56:08 +0530

    Update GeneralKnowledge.js

--- src/QuizPages/GeneralKnowledge.js
+++ src/QuizPages/GeneralKnowledge.js
@@ -131,7 +131,12 @@ const GeneralKnowledge = () => {
                 // const jsDate = new Date().toISOString().slice(0, 19).replace('T', ' ');
                 const jsDate = new Date().toISOString().slice(0, 19).replace('T', ' ');
                 console.log('Date: ',jsDate);
-                axios.post("https://techie-webapp-api.onrender.com/savescores",{name: user.name, email: user.email, score: score, category: questions[0].category, date: jsDate}).then(res=>console.log(res.data))
+                const saveScores = async() =>{
+                    const response = await axios.post("https://techie-webapp-api.onrender.com/savescores",{name: user.name, email: user.email, score: score, category: questions[0].category, date: jsDate});
+                    console.log(response.data);
+                }
+                saveScores();
+                // axios.post("https://techie-webapp-api.onrender.com/savescores",{name: user.name, email: user.email, score: score, category: questions[0].category, date: jsDate}).then(res=>console.log(res.data))
                 // console.log('set score: ',score);
                 // navigate('/dashboard2/taketest/scoreboard',{state:{score: score, category: questions[0].category}});
             },3000);
